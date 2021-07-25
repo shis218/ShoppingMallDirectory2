fbe6b2539d34bbf5059ffa86e005d7310444b4fa
Henrique Nakaema Simoes <31418210+shis218@users.noreply.github.com>
2021-07-25 06:19:50 -0300

    Initial commit

---
+++ src/main/java/com/example/ShoppingMallDirectory2/ShoppingMallDirectory2Application.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingMallDirectory2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallDirectory2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallDirectory2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShoppingMallDirectory2/ShoppingMallDirectory2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingMallDirectory2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallDirectory2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
